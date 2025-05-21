d902c39a3656f1602c3ff591022ff30daa262ec8
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-05-21 10:56:06 -0500

    feat: define JPA entity UserEntity

---
+++ src/main/java/com/invoice_management/Entities/UserEntity.java
@@ -0,0 +1,38 @@
+package com.invoice_management.Entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "Users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String email;
+    private String name;
+
+    private String password;
+    @Column(name =  "contact_number")
+    private String contacNumber;
+
+    private String role;
+    private String status;
+
+
+
+}
