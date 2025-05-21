37fed2c99a90b16c5c19d23bcc22fdd9851abd67
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-05-21 10:05:52 -0500

    feat: initialize project

---
+++ src/main/java/com/invoice_management/ApiFacturasApplication.java
@@ -0,0 +1,13 @@
+package com.invoice_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFacturasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFacturasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nohemi/api_facturas/ApiFacturasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nohemi.api_facturas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFacturasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
